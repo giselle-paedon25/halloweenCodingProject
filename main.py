9baa41ff9f08301e7a6d632f8bdcc8cf48f41aa4
giselle-paedon25 <91690159+giselle-paedon25@users.noreply.github.com>
2021-11-01 13:56:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
+#Importing Turtle and Random modules
 import turtle as trtl
 import random as rand
+
+
+#Creating screen and background
+wn = trtl.Screen()
+wn.bgpic("halloween.png")
+
 #creating candy object
 candy = trtl.Turtle()
-wn = trtl.Screen()
-candies = ["zTwix.gif, ghost1.gif, ghost2.gif"]
+#Creating Candies List
+candies = ['zTwix.gif', 'ghost1.gif', 'ghost2.gif', 'giphy.gif', 'skittles.gif', 'snickers.gif', ]
 
-def function(x):
-  wn.addshape(x)
-  candy.shape(x)
-function(candies)
+wn.addshape('zTwix.gif')
+candy.shape('zTwix.gif')
 
+num = len(candies) - 1
+
+#Changing location of Candy
 def pos_change():
   candy.penup()
   new_xpos = rand.randint(-200,200)
   new_ypos = rand.randint(-200,200)
   candy.goto(new_xpos, new_ypos)
 
+candy.score = 0
+
+# randomizes the candy being clicked
 def candy_clicked(x,y):
   pos_change()
+  num2 = rand.randint(0,num)
+  candies2 = candies[num2]
+  wn.addshape(candies2)
+  candy.shape(candies2)
+  candy.score += 5
+  print(candy.score)
 
+# running the function
 candy.onclick(candy_clicked)
-#wn.bgpic("halloween.jpg")
+
+
+
+#Window
 wn.mainloop()
